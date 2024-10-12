89ce0f2c2ec392879b621d04f8bd1343f5911a07
Emma Roy <e664r593@home.ku.edu>
2024-10-12 17:26:42 -0500

    added in 6  month moving report

--- code_one.c
+++ code_one.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h> 
 int main()
 {
     char filename[100];
@@ -15,13 +16,43 @@ int main()
         perror("Error opening file");
         return 0;   
         }
-
-    printf("Monthly Sales Report for 2024\n\n","%-10s %-10s\n", "Month", "Sales");
+    float month_max = FLT_MIN;
+    float month_min = FLT_MAX;
+    float avg = 0;
+    float month_avg[7] = {0};
+    int max_index,min_index = -1;
+    printf("\nMonthly Sales Report for 2024\n\n","%-10s %-10s\n", "Month", "Sales");
     for (int i = 0; i < 12; i++) {
         fscanf(file, "%f", &monthly_sales[i]);
-        printf("%-10s %-10f\n", months[i], monthly_sales[i]);
+        
+        if(monthly_sales[i] > month_max){
+            month_max = monthly_sales[i];
+            max_index = i;
+        }
+         if(monthly_sales[i] < month_min){
+            month_min = monthly_sales[i];
+            min_index = i;
+         }
+        if (i >= 0 && i <= 5) month_avg[0] += monthly_sales[i]; // Jan to June
+        if (i >= 1 && i <= 6) month_avg[1] += monthly_sales[i]; // Feb to July
+        if (i >= 2 && i <= 7) month_avg[2] += monthly_sales[i]; // March to Aug
+        if (i >= 3 && i <= 8) month_avg[3] += monthly_sales[i]; // April to Sept
+        if (i >= 4 && i <= 9) month_avg[4] += monthly_sales[i]; // May to Oct
+        if (i >= 5 && i <= 10) month_avg[5] += monthly_sales[i]; // June to Nov
+        if (i >= 6 && i <= 11) month_avg[6] += monthly_sales[i]; // July to Dec
+        printf("%-10s %-10.2f\n", months[i], monthly_sales[i]);
+        avg = (avg+ monthly_sales[i]);
+        }
+    printf("\nSales Summary Report:\n");
+    printf("Highest Sales: %.2f (%s)\n", month_max, months[max_index]); 
+    printf("Lowest Sales: %.2f (%s)\n", month_min, months[min_index]);
+    printf("Average Sales: %.2f\n", avg/12);
+    printf("\nSix-Month moving average report:\n\n");
+    for (int i = 0; i < 7; i++) {
+    printf("%-10s %-10.2f\n", months[i], month_avg[i] / 6); // Divide by 6 for the average
     }
     fclose(file);
     return 0;
-
     }
+
+
